55c8f747b7440f6050aff8409fe9b2af73323057
gauravm-bit <gauravwrk01@gmail.com>
2020-04-05 16:38:52 +0530

    ADDED: TC1.5 to correct file but incorrect header.

--- src/main/java/service/StateCensusAnalyser.java
+++ src/main/java/service/StateCensusAnalyser.java
@@ -10,7 +10,6 @@ import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.Iterator;
 
-
 public class StateCensusAnalyser {
     private static String CSV_FILE_PATH;
     int count=0;
